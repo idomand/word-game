48214c4c388e025715f5932f1813516cb6dda4b7
Ido Mandelman <idomand@gmail.com>
2023-04-18 21:35:22 +0200

    creating a redux store and slice

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,24 @@ import AllWords from "./Components/AllWords";
 import ScoreBoard from "./Components/ScoreBoard";
 import GameContextComponent from "./lib/GameContextComponent";
 import GameBoard from "./Components/GameBoard";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 function App() {
   return (
     <GameContextComponent>
-      <BrowserRouter>
-        <div className="min-h-screen bg-gradient-to-r from-cyan-500 to-indigo-500	">
-          <Header />
-          <Routes>
-            <Route path="/allWords" element={<AllWords />} />
-            {/* <Route path="/score-board" element={<ScoreBoard />} /> */}
-            <Route path="/" element={<GameBoard />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="min-h-screen bg-gradient-to-r from-cyan-500 to-indigo-500	">
+            <Header />
+            <Routes>
+              <Route path="/allWords" element={<AllWords />} />
+              {/* <Route path="/score-board" element={<ScoreBoard />} /> */}
+              <Route path="/" element={<GameBoard />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </Provider>
     </GameContextComponent>
   );
 }
---
+++ src/Redux/GameDataSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import data from "../German-words-data.json";
+
+const randomStartingIndex = Math.floor(Math.random() * 1000);
+export const firstWord = data[randomStartingIndex];
+
+const initialState: GlobalState = {
+  wordObject: firstWord,
+  score: 0,
+  userName: "ido",
+  indexOfWordInArray: 0,
+  indexOfWordInAllData: 0,
+  arrayOfWords: [],
+  isGameStated: false,
+  isGameEnded: false,
+  arrayOfWordsWrongAnswer: [],
+  arrayOfWordsRightAnswer: [],
+};
+
+// const GameReducer = () => {};
+
+export const GameDataSlice = createSlice({
+  name: "gameData",
+  initialState: initialState,
+  reducers: {
+    GetArrayOfWord: (state, action) => {
+      const { numberOfWords, startingIndex } = action.payload;
+      state.indexOfWordInAllData = startingIndex;
+      const arrayOfWords = [];
+      for (let i = startingIndex; i < startingIndex + numberOfWords; i++) {
+        arrayOfWords.push(data[i]);
+      }
+      state.wordObject = arrayOfWords[0];
+      state.isGameStated = true;
+      state.arrayOfWords = arrayOfWords;
+    },
+  },
+});
+
+// export const {} = GameDataSlice.actions;
+export const GameReducer = GameDataSlice.reducer;
+export default GameDataSlice;
