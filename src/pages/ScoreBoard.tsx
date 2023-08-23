4ba8cf3e158ebf99114ad3e457f3e55b5af34f84
Ido Mandelman <idomand@gmail.com>
2023-08-23 13:16:23 +0200

    basic toggle button

--- src/pages/ScoreBoard.tsx
+++ src/pages/ScoreBoard.tsx
@@ -3,6 +3,8 @@ import { getScoreBoard } from "../Firebase/FirebaseCrud";
 import { topScoreDataType } from "../global";
 
 export default function ScoreBoard() {
+  const [showPersonalScore, setShowPersonalScore] = useState(true);
+  console.log("showPersonalScore", showPersonalScore);
   const [currentData, setCurrentData] = useState<topScoreDataType[]>([
     {
       name: "",
@@ -18,22 +20,40 @@ export default function ScoreBoard() {
     );
     setCurrentData(sortedData);
   }
-  console.log("currentData", currentData);
   useEffect(() => {
     fetchData();
   }, []);
-  console.log("currentData", currentData);
   return (
     <section>
-      <h1 className="text-center underline">Score Board</h1>
+      <div className="mx-auto w-fit">
+        <label
+          htmlFor="Toggle3"
+          className="inline-flex cursor-pointer items-center rounded-md p-2 dark:text-gray-800"
+        >
+          <input
+            id="Toggle3"
+            type="checkbox"
+            className="peer hidden"
+            checked={showPersonalScore}
+            onChange={(e) => {
+              setShowPersonalScore(() => !showPersonalScore);
+            }}
+          />
+          <span className="rounded-l-md bg-violet-400 px-4 py-2 peer-checked:bg-gray-300">
+            Personal Best
+          </span>
+          <span className="rounded-r-md bg-gray-300 px-4 py-2 peer-checked:bg-violet-400">
+            Top Score
+          </span>
+        </label>
+      </div>
+
       <div className="mx-auto mt-3 w-fit ">
         {currentData &&
           currentData.map((data: any) => {
             return (
-              <div className="flex justify-between">
-                <p className="" key={data.name}>
-                  {data.name}
-                </p>
+              <div className="flex justify-between" key={data.name}>
+                <p className="">{data.name}</p>
                 <p className="ml-3"> {data.topScore}</p>
               </div>
             );
